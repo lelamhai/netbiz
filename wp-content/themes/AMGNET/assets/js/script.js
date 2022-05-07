3cbefeae8dd7f9d3e34dc6bf10b9743aac1cd9a1
lelamhai <leelamhair@gmail.com>
2022-05-07 17:23:39 +0700

    fix bug scroll

--- wp-content/themes/AMGNET/assets/js/script.js
+++ wp-content/themes/AMGNET/assets/js/script.js
@@ -1,2 +1,17 @@
-//go to top button
-$(window).load(function () {$(".gotop").hide();$(function () {$(window).scroll(function () {if ($(this).scrollTop() > ($(".webpage").offset().top)) {$('.gotop').fadeIn();} else {$('.gotop').fadeOut();}});$('.gotop').click(function () {$('body,html').animate({scrollTop: $(".webpage").offset().top}, 500);return false;});});});
+ //go to top button
+ $(window).load(function () {
+    $(".gotop").hide();
+    $(function () {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > $(".webpage").offset().top) {
+          $(".gotop").fadeIn();
+        } else {
+          $(".gotop").fadeOut();
+        }
+      });
+      $(".gotop").click(function () {
+        $("body,html").animate({ scrollTop: $(".webpage").offset().top }, 500);
+        return false;
+      });
+    });
+  });
